352bf145e94b91a363871c4c9eb13d9342586472
Vlisto <66323620+Vlisto@users.noreply.github.com>
2020-06-11 06:41:12 +0100

    having issues with minimized classes dropdown

--- main.js
+++ main.js
@@ -3,8 +3,21 @@ const mobileMenu = document.querySelector(".mobile-menu");
 const menuClose=document.querySelector("#menu-close");
 const ul = document.querySelector("ul");
 
+const expand=document.querySelector("#expand");
+const collapse=document.querySelector("#collapse");
+const mobileEx=document.querySelector(".mobile-ex");
+const expandul=document.querySelector(".classes ul");
+
+
 mobileMenu.addEventListener("click", function(){
 ul.classList.toggle("open");
 menuClose.classList.toggle("open");
 menu.classList.toggle("open");
+});
+
+mobileEx.addEventListener("click", function(){
+    expandul.classList.toggle("expand");
+    expand.classList.toggle("expand");
+    collapse.classList.toggle("expand");
+    
 });
